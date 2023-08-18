c89a7c4c02946623d1e5a3cb9acc2862e9a419fd
drsimplegraffiti <abayomiogunnusi@gmail.com>
2023-08-18 21:39:27 +0100

    initial commit

---
+++ Requests/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TasksApi.Requests
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+    }
+}
---
+++ Responses/BaseResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TasksApi.Responses
+{
+    public abstract class BaseResponse
+    {
+        [JsonIgnore()]
+        public bool Success { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] //this will not serialize the property if it is null
+        public string ErrorCode { get; set; } = String.Empty;
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string Error { get; set; } = String.Empty;
+    }
+
+}
